42b42020f68a4bc8640801925916b69c8c50f73a
felim <s221516@dtu.dk>
2026-03-26 16:19:16 +0100

    hep

--- src/main/java/com/transactionapp/transactionsorter/TransactionSorterApplication.java
+++ src/main/java/com/transactionapp/transactionsorter/TransactionSorterApplication.java
@@ -2,12 +2,9 @@ package com.transactionapp.transactionsorter;
 
 import com.transactionapp.transactionsorter.BucketService.Bucket;
 import com.transactionapp.transactionsorter.BucketService.BucketRepository;
-import com.transactionapp.transactionsorter.BucketService.BucketService;
-import com.transactionapp.transactionsorter.BucketService.TransactionAddedToBucketEvent;
 import com.transactionapp.transactionsorter.TransactionCategorizationService.TransactionCategorizationService;
 import com.transactionapp.transactionsorter.TransactionService.Transaction;
 import com.transactionapp.transactionsorter.TransactionService.TransactionRepository;
-import com.transactionapp.transactionsorter.TransactionService.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,12 +15,28 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 public class TransactionSorterApplication {
+
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = SpringApplication.run(TransactionSorterApplication.class, args);
 
         TransactionCategorizationService service = context.getBean(TransactionCategorizationService.class);
         TransactionRepository trepo = context.getBean(TransactionRepository.class);
         BucketRepository brepo = context.getBean(BucketRepository.class);
+
+        for (int i = 0; i < 5; i++) {
+            createTransaction(trepo, brepo);
+        }
+    }
+
+    private static void createTransaction(TransactionRepository trepo, BucketRepository brepo) {
+        int k = 50 * 12 * 3;
+        Bucket bucket = new Bucket("forkem");
+        brepo.save(bucket);
+        for (int i = 1; i <= k; i++) {
+            Transaction t = new Transaction("cock", LocalDate.parse("2026-01-01"), new BigDecimal("100.00"));
+            trepo.save(t);
+            bucket.addTransaction(t);
+        }
     }
 }
 
